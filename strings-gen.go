9f78a0e9a575843c812e34d6e437d2b2b6879f39
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-03-08 11:37:21 +0100

    Added StringUppercaseAlphaNumericSource variable to randgen package.

--- strings-gen.go
+++ strings-gen.go
@@ -36,8 +36,12 @@ var StringSymbolsSource string = "~`!@#$%^&*()_+-={}[]|\\;:'\",.<>/?"
 var StringNumbericSoure string = "0123456789"
 
 // StringLowercaseAlphaNumericSource is a string generator source that contains
-// lowercase alph-numerice characters only.
+// lowercase alph-numeric characters only.
 var StringLowercaseAlphaNumericSource string = StringLowercaseAlphabetsSource + StringNumbericSoure
+
+// StringUppercaseAlphaNumericSource is a string generator source that contains
+// uppercase alph-numeric characters only.
+var StringUppercaseAlphaNumericSource string = StringUppercaseAlphabetsSource + StringNumbericSoure
 
 // NewStringGenerator returns a new string generator object that
 // implements the StringGenerator interface.
